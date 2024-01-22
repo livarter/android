35effe7b019cf4167ddb7db7f6d5da688f86290c
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-22 14:09:24 +0900

    feat: Product data클래스 생성

---
+++ app/src/main/java/net/developia/livartc/model/Product.kt
@@ -0,0 +1,24 @@
+package net.developia.livartc.model
+
+import java.util.Date
+
+/**
+ * LIVARTC
+ * Created by 최현서
+ * Date: 2024-01-22
+ * Time: 오후 12:36
+ */
+data class Product(
+//    val id: Int,
+//    val brandId: Int,
+    val name: String,
+    val price: Int,
+//    val description: String,
+//    val createdAt: Date,
+//    val updatedAt: Date,
+//    val category: String,
+//    val isBest: Int,
+//    val viewCnt: Int,
+//    val orderCnt: Int,
+    val productImg: String
+)
