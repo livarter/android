e93f3252776671c1f19d93c7214960a17cc155ec
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-23 10:09:51 +0900

    bug: 다른 액티비티갔다오면 메인베너 속도 빨라지는 버그 수정

--- app/src/main/java/net/developia/livartc/main/HomeFragment.kt
+++ app/src/main/java/net/developia/livartc/main/HomeFragment.kt
@@ -31,9 +31,9 @@ class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
     private var currentPage = 0
     private var bestList: BestProduct? = null
-    lateinit var mainActivity: MainActivity
-    lateinit var bestAdapter: BestProductAdapter
-
+    private lateinit var mainActivity: MainActivity
+    private lateinit var bestAdapter: BestProductAdapter
+    private var isAutoScrolling = false
     val handler = Handler(Looper.getMainLooper()) {
         setPage()
         true
@@ -41,8 +41,6 @@ class HomeFragment : Fragment() {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-
-        // 2. Context를 Activity로 형변환하여 할당
         mainActivity = context as MainActivity
     }
 
@@ -50,38 +48,47 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //배너 뷰페이저2 세팅하는 함수
+        getAllBestProduct()
         makeBanner()
 
-        getAllBestProduct()
-
-
-        //category버튼 누르면 CategoryFragment로 변경
         binding.categoryBtn.setOnClickListener {
             parentFragmentManager.beginTransaction().apply {
                 replace(R.id.main_container, CategoryFragment())
-                //addToBackStack(null)메서드는, 프레그먼트를 replace했을때, 뒤로가기를 가능하게 하는 함수
                 addToBackStack(null)
                 commit()
             }
         }
+
         val bundle = Bundle()
         bundle.putString("title", "베스트상품")
-        //베스트 조회 눌렀을 때 ProductActivity 생성(베스트순으로 필터링된 조회창으로 이동)
+
         binding.bestBtn.setOnClickListener {
             (activity as MainActivity).startProductActivity("베스트 상품")
         }
     }
 
-    private fun getAllBestProduct() {
+    override fun onPause() {
+        super.onPause()
+        stopBannerAutoScroll()
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        stopBannerAutoScroll()
+    }
+
+    private fun stopBannerAutoScroll() {
+        isAutoScrolling = false
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    private fun getAllBestProduct() {
         Thread {
             val networkService = (context?.applicationContext as MyApplication).networkService
             var getBestCall = networkService.getProduct()
@@ -100,59 +107,51 @@ class HomeFragment : Fragment() {
     }
 
     private fun setBestRecyclerView() {
-        mainActivity.runOnUiThread{
+        mainActivity.runOnUiThread {
             bestAdapter = BestProductAdapter(bestList, this@HomeFragment)
             binding.recyclerView.adapter = bestAdapter
             binding.recyclerView.layoutManager = GridLayoutManager(activity, 2)
         }
     }
 
+    private fun makeBanner() {
+        val bannerAdapter = HomeBannerAdapter(this)
+        bannerAdapter.addFragment(BannerFragment01())
+        bannerAdapter.addFragment(BannerFragment02())
+        bannerAdapter.addFragment(BannerFragment03())
 
-private fun makeBanner() {
-    val bannerAdapter = HomeBannerAdapter(this)
-    bannerAdapter.addFragment(BannerFragment01())
-    bannerAdapter.addFragment(BannerFragment02())
-    bannerAdapter.addFragment(BannerFragment03())
+        val viewPager: ViewPager2 = binding.viewPager2Container
 
-    val viewPager: ViewPager2 = binding.viewPager2Container
-
-    viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-        override fun onPageSelected(position: Int) {
-            super.onPageSelected(position)
-            // 페이지가 선택될 때마다 페이지 번호 업데이트
-            binding.pageNum.text = "${position + 1}  /  3"
-            currentPage = position
-        }
-    })
-
-    binding.viewPager2Container.adapter = bannerAdapter
-    binding.viewPager2Container.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-    val child = binding.viewPager2Container.getChildAt(0)
-    //스크롤 넘길 때, 효과 나타 나지 않도록
-    (child as? RecyclerView)?.overScrollMode = View.OVER_SCROLL_NEVER
-
-    val thread = Thread(PagerRunnable())
-    thread.start()
-}
-
-private fun setPage() {
-    if (currentPage == 3) currentPage = 0
-    binding.viewPager2Container.setCurrentItem(currentPage, true)
-    currentPage += 1
-}
-
-inner class PagerRunnable : Runnable {
-    override fun run() {
-        while (true) {
-            try {
-                Thread.sleep(2500)
-                handler.sendEmptyMessage(0)
-            } catch (e: InterruptedException) {
-                Log.d("interupt", "interupt발생")
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.pageNum.text = "${position + 1}  /  3"
+                currentPage = position
             }
+        })
+
+        binding.viewPager2Container.adapter = bannerAdapter
+        binding.viewPager2Container.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val child = binding.viewPager2Container.getChildAt(0)
+        (child as? RecyclerView)?.overScrollMode = View.OVER_SCROLL_NEVER
+
+        isAutoScrolling = true
+        handler.postDelayed(PagerRunnable(), 2500)
+    }
+
+    inner class PagerRunnable : Runnable {
+        override fun run() {
+            if (!isAutoScrolling) {
+                return
+            }
+            setPage()
+            handler.postDelayed(this, 2500)
         }
     }
-}
 
-
+    private fun setPage() {
+        if (currentPage == 3) currentPage = 0
+        binding.viewPager2Container.setCurrentItem(currentPage, true)
+        currentPage += 1
+    }
 }
