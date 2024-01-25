a45757791541eafaa1a13c4d586099edfdb864a4
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-26 08:01:54 +0900

    feat: Search에서 Detail로 데이터 옮김

--- app/src/main/java/net/developia/livartc/product/DetailFragment.kt
+++ app/src/main/java/net/developia/livartc/product/DetailFragment.kt
@@ -1,6 +1,7 @@
 package net.developia.livartc.product
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,9 +10,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import net.developia.livartc.ProductActivity
+import net.developia.livartc.PurchaseActivity
+import net.developia.livartc.ReplyWriteActivity
 import net.developia.livartc.adapter.ReplyAdapter
 import net.developia.livartc.databinding.FragmentDetailBinding
+import net.developia.livartc.model.Product
 import net.developia.livartc.model.Reply
 import net.developia.livartc.retrofit.RetrofitInstance
 import retrofit2.Call
@@ -21,6 +26,7 @@ import retrofit2.Response
 /**
  * LIVARTC
  * Created by 변형준
+ * Enhanced by 최현서
  * Date: 1/19/24
  * Time: 17:21
  */
@@ -40,14 +46,6 @@ class DetailFragment : Fragment() {
         }
         return binding.root
     }
-
-
-    /**
-     * LIVARTC
-     * Added by 최현서
-     * Date: 1/25/24
-     * Time: 11:42
-     */
     override fun onAttach(context: Context) {
         super.onAttach(context)
         productActivity = context as ProductActivity
@@ -56,6 +54,19 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getAllReply()
+        binding.replyWriteBtn.setOnClickListener {
+            val intent = Intent(activity, ReplyWriteActivity::class.java)
+            startActivity(intent)
+        }
+        val product = arguments?.getSerializable("product") as? Product
+        if (product != null) {
+            Glide.with(binding.productImg.context)
+                .load(product.productImage)
+                .into(binding.productImg)
+            binding.productName.text = product.productName
+            binding.productDesc.text = product.productDescription
+            binding.productPrice.text = "${product.productPrice}"
+        }
     }
 
     //베스트 상품 조회 관련(Retrofit 연동 후 recycler view 뿌림)
