7259b1ae955b31d48734ed7f30f804061827af93
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-26 08:02:25 +0900

    feat: 리뷰 작성 페이지

--- app/src/main/java/net/developia/livartc/ReplyWriteActivity.kt
+++ app/src/main/java/net/developia/livartc/ReplyWriteActivity.kt
@@ -2,6 +2,10 @@ package net.developia.livartc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
 import net.developia.livartc.databinding.ActivityReplyWriteBinding
 
 class ReplyWriteActivity : AppCompatActivity() {
@@ -9,7 +13,43 @@ class ReplyWriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReplyWriteBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
+        with(binding){
+             reviewWrite.addTextChangedListener(object : TextWatcher {
+                var maxText = ""
+                override fun beforeTextChanged(pos: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    maxText = pos.toString()
+                }
+                override fun onTextChanged(pos: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                    if(reviewWrite.lineCount > 4){
+                        Toast.makeText(this@ReplyWriteActivity,
+                            "최대 4줄까지 입력 가능합니다.",
+                            Toast.LENGTH_SHORT).show()
+
+                        reviewWrite.setText(maxText)
+                        reviewWrite.setSelection(reviewWrite.length())
+                        reviewTextCnt.text = ("${reviewWrite.length()} / 40")
+                    } else if(reviewWrite.length() > 40){
+                        Toast.makeText(this@ReplyWriteActivity, "최대 40자까지 입력 가능합니다.",
+                            Toast.LENGTH_SHORT).show()
+
+                        reviewWrite.setText(maxText)
+                        reviewWrite.setSelection(reviewWrite.length())
+                        reviewTextCnt.text = ("${reviewWrite.length()} / 40")
+                    } else {
+                        reviewTextCnt.text = ("${reviewWrite.length()} / 40")
+                    }
+                }
+                override fun afterTextChanged(p0: Editable?) {
+
+                }
+            })
+        }
+
+        binding.saveBtn.setOnClickListener {
+            Log.d("hschoi", binding.reviewWrite.text.toString())
+        }
     }
 }
