f0718b0b2c7dc1c739fd2d9912cefe7ffa75b00b
osy <osy9757@gmail.com>
2024-01-25 20:35:36 +0900

    feat : brand에 따른 hashtag, brand&hashtag 검색기능 추가, sorting 선택에 따른 적용 구현

--- app/src/main/java/net/developia/livartc/product/SearchFragment.kt
+++ app/src/main/java/net/developia/livartc/product/SearchFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.AdapterView
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +17,7 @@ import net.developia.livartc.adapter.HashTagAdapter
 import net.developia.livartc.adapter.ProductAdapter
 import net.developia.livartc.databinding.FragmentSearchPageBinding
 import net.developia.livartc.databinding.FragmentSearchSearchbarBinding
+import net.developia.livartc.databinding.FragmentSearchSortingdropdownBinding
 import net.developia.livartc.model.Product
 import net.developia.livartc.retrofit.RetrofitInstance
 import retrofit2.Call
@@ -25,6 +28,12 @@ class SearchFragment : Fragment() {
     lateinit var binding: FragmentSearchPageBinding
     lateinit var searchBarBinding: FragmentSearchSearchbarBinding
     private var selectedBrand: String? = null
+    private var selectedHashTag: String? = null
+    private lateinit var products: List<Product>
+    private var selectedSortOption: Int = 4
+    private lateinit var sortingSpinner: Spinner
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +51,7 @@ class SearchFragment : Fragment() {
         searchBarBinding.searchViews.isSubmitButtonEnabled = true
         searchBarBinding.searchViews.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                searchProducts(query ?:"",selectedBrand)
+                searchProducts(query ?: "", selectedBrand, selectedHashTag, selectedSortOption)
                 return true
             }
 
@@ -54,16 +63,18 @@ class SearchFragment : Fragment() {
         })
     }
 
-    private fun searchProducts(searchQuery: String, selectedBrand: String?) {
+    private fun searchProducts(searchQuery: String, selectedBrand: String?, hashTag: String?=null, sortOption: Int) {
 
-        val query = searchQuery.ifBlank { null }
+        val query = searchQuery.ifBlank { "" }
 
-        RetrofitInstance.api.searchProducts("", selectedBrand ?: "","", query ?:"", 1, 10, 1)
+        RetrofitInstance.api.searchProducts("", selectedBrand ?: "", hashTag ?: "", query ?: "", sortOption, 10, 1)
             .enqueue(object : Callback<List<Product>> {
                 override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
                     if (response.isSuccessful) {
                         val products = response.body() ?: emptyList()
+                        this@SearchFragment.products = products
                         displaySearchResults(products)
+                        displaySelectedBrandHashTags(selectedBrand)
 
                         // 로그에 결과 출력
                         Log.d("SearchFragment", "검색 결과: $products")
@@ -79,6 +90,8 @@ class SearchFragment : Fragment() {
     }
 
     private fun displaySearchResults(products: List<Product>) {
+        this.products = products
+
         val recyclerView = binding.productRecyclerView
         recyclerView.layoutManager = GridLayoutManager(context, 2) // 2열 그리드 레이아웃 설정
         recyclerView.adapter = ProductAdapter(products) { product ->
@@ -89,29 +102,52 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        sortingSpinner = view.findViewById(R.id.spinnerSortOptions) // 여기서 ID는 spinner의 ID와 일치해야 합니다.
+        setupSpinner()
+
         loadInitialProducts()
 
         val brandNames = resources.getStringArray(R.array.brand_names).toList()
         val brandAdapter = BrandAdapter(brandNames) { brand ->
-            selectedBrand = brand
-            // selectedBrand의 null 여부를 확인하고, null이 아닌 경우에만 함수 호출
+            selectedBrand = if (selectedBrand == brand) null else brand
+            displaySelectedBrandHashTags(selectedBrand)
             searchBarBinding.searchViews.query?.toString()?.let { query ->
-                searchProducts(query, selectedBrand)
+                searchProducts(query, selectedBrand,"",selectedSortOption)
             }
         }
         binding.searchBrandhashtag.brandsRecyclerView.adapter = brandAdapter
         binding.searchBrandhashtag.brandsRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
-        val hashTags = listOf("해시태그1", "해시태그2", "해시태그3") // 임시 데이터
-        val hashTagAdapter = HashTagAdapter(hashTags)
-        binding.searchBrandhashtag.hashTagRecyclerView.adapter = hashTagAdapter
-        binding.searchBrandhashtag.hashTagRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-
-
     }
 
+    private fun setupSpinner() {
+        sortingSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedSortOptionString = parent.getItemAtPosition(position) as String
+                selectedSortOption = when (selectedSortOptionString) {
+                    "최신순" -> 4
+                    "이름순" -> 3
+                    "낮은가격순" -> 1
+                    "높은가격순" -> 2
+                    "판매량순" -> 5
+                    "조회수순" -> 6
+                    else -> 4 // 기본값
+                }
+                Log.d("SearchFragment", "선택된 정렬 옵션: $selectedSortOption")
+                // 선택된 정렬 옵션에 따른 추가 작업
+                searchProducts("", selectedBrand, selectedHashTag, selectedSortOption)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // 선택되지 않았을 때의 동작
+            }
+        }
+    }
+
+
     private fun loadInitialProducts() {
-        searchProducts("",selectedBrand)
+        searchProducts("",selectedBrand,"",selectedSortOption)
+        displaySelectedBrandHashTags(selectedBrand)
     }
 
     private fun showProductDetail(product: Product) {
@@ -126,6 +162,29 @@ class SearchFragment : Fragment() {
         fragmentManager?.beginTransaction()?.replace(R.id.product_container, detailFragment)?.addToBackStack(null)?.commit()
 
     }
+
+    private fun displaySelectedBrandHashTags(selectedBrand: String?) {
+        if (selectedBrand != null) {
+            val brandProducts = products.filter { it.brandName == selectedBrand }
+            if (brandProducts.isNotEmpty()) {
+                val hashTags = brandProducts.first().allHashtags.split(", ").take(6)
+                val hashTagAdapter = HashTagAdapter(hashTags) { hashTag ->
+                    // 해시태그 클릭시 처리될 로직
+                    searchProducts("", selectedBrand, hashTag,selectedSortOption)
+                    Log.d("hashTags", "$hashTags")
+                }
+                binding.searchBrandhashtag.hashTagRecyclerView.adapter = hashTagAdapter
+                binding.searchBrandhashtag.hashTagRecyclerView.layoutManager =
+                    LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+                binding.searchBrandhashtag.hashTagRecyclerView.visibility = View.VISIBLE
+            } else {
+                binding.searchBrandhashtag.hashTagRecyclerView.visibility = View.GONE
+            }
+        } else {
+            binding.searchBrandhashtag.hashTagRecyclerView.visibility = View.GONE
+        }
+    }
+
 }
 
 
