2e129f2a9fe656884962851bf184834be2e2b336
osy <osy9757@gmail.com>
2024-01-27 17:43:36 +0900

    브랜드 클릭 이벤트 수정, searchFragment 전체 로직 수정, 해시태그 추가

--- app/src/main/java/net/developia/livartc/adapter/HashTagAdapter.kt
+++ app/src/main/java/net/developia/livartc/adapter/HashTagAdapter.kt
@@ -1,5 +1,8 @@
 package net.developia.livartc.adapter
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,8 +17,11 @@ import net.developia.livartc.R
  * Time: 5:22 PM
  */
 class HashTagAdapter(private val hashTags: List<String>,
-                     private val onItemClicked: (String) -> Unit) :
+                     private val onClick: (String) -> Unit) :
     RecyclerView.Adapter<HashTagAdapter.HashTagViewHolder>() {
+
+    private var selectedPosition = RecyclerView.NO_POSITION
+
 
     class HashTagViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val hashTagText: TextView = view.findViewById(R.id.hashTagText)
@@ -26,11 +32,32 @@ class HashTagAdapter(private val hashTags: List<String>,
         return HashTagViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: HashTagViewHolder, position: Int) {
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onBindViewHolder(holder: HashTagViewHolder, @SuppressLint("RecyclerView") position: Int) {
         val hashTag = hashTags[position]
         holder.hashTagText.text = hashTag
-        holder.itemView.setOnClickListener { onItemClicked(hashTag) }
+
+        // 선택된 해시태그 스타일 변경
+        if (position == selectedPosition) {
+            holder.hashTagText.setTextColor(Color.parseColor("#000000")) // 선택된 해시태그 색상
+            holder.hashTagText.setTypeface(null, Typeface.BOLD) // Bold 스타일 적용
+        } else {
+            holder.hashTagText.setTextColor(Color.parseColor("#495057")) // 기본 해시태그 색상
+            holder.hashTagText.setTypeface(null, Typeface.NORMAL) // 기본 스타일 적용
+        }
+
+        holder.itemView.setOnClickListener {
+            if(selectedPosition == position) {
+                selectedPosition = RecyclerView.NO_POSITION
+                onClick("")
+            } else {
+                selectedPosition = position
+                onClick(hashTag)
+            }
+            notifyDataSetChanged()
+        }
     }
+
 
     override fun getItemCount() = hashTags.size
 }
