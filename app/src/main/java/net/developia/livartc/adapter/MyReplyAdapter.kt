8b045975203832a1b9a3d55b9f9f96cfd0dea3dc
최현서 <117807082+ha01032@users.noreply.github.com>
2024-01-28 22:50:00 +0900

    feature: 나의 리뷰 조회

---
+++ app/src/main/java/net/developia/livartc/adapter/MyReplyAdapter.kt
@@ -0,0 +1,61 @@
+package net.developia.livartc.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import net.developia.livartc.databinding.ItemMyreviewBinding
+import net.developia.livartc.databinding.ItemReplyBinding
+import net.developia.livartc.model.MyReply
+import net.developia.livartc.model.Reply
+import net.developia.livartc.retrofit.MyApplication
+
+/**
+ * LIVARTC
+ * Created by 최현서
+ * Date: 1/25/24
+ * Time: 11:48
+ */
+class MyReplyAdapter(private val myReplyList: List<MyReply>) :
+    RecyclerView.Adapter<MyReplyAdapter.MyReplyViewHolder>() {
+
+    class MyReplyViewHolder(val binding: ItemMyreviewBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyReplyViewHolder {
+        val binding = ItemMyreviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyReplyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyReplyViewHolder, position: Int) {
+        val myReplyItem = myReplyList[position]
+        Log.d("MyReplyAdapter:", "$position : ${myReplyList[position]}")
+        holder.binding.profileNickname.text = myReplyItem.nickname
+        holder.binding.replyContent.text = myReplyItem.replyComment.replace("\\n", "\n")
+        holder.binding.replyDate.text = "${myReplyItem.createdAt.year}.${String.format("%02d", myReplyItem.createdAt.monthValue)}.${String.format("%02d", myReplyItem.createdAt.dayOfMonth)}"
+        holder.binding.productName.text = myReplyItem.productName
+        holder.binding.productBrand.text = myReplyItem.productBrand
+        Glide.with(holder.binding.productImg.context)
+            .load(myReplyItem.productImage)
+            .into(holder.binding.productImg)
+
+        if (myReplyItem.replyImg != null) {
+            val imgRef = MyApplication.storage.reference.child("review/${myReplyItem.replyImg}")
+            imgRef.downloadUrl.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Glide.with(holder.binding.replyImage.context)
+                        .load(task.result)
+                        .into(holder.binding.replyImage)
+                    holder.binding.replyImage.visibility = View.VISIBLE
+                } else {
+                    Log.d("ReplyAdapter:", "Firebase 이미지 다운로드 실패")
+                    holder.binding.replyImage.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+    override fun getItemCount() = myReplyList.size
+}
---
+++ app/src/main/java/net/developia/livartc/model/MyReply.kt
@@ -0,0 +1,61 @@
+package net.developia.livartc.model
+
+/**
+ * LIVARTC
+ * Created by 최현서
+ * Date: 2024-01-28
+ * Time: 오후 10:08
+ */
+data class MyReply(
+    val createdAt: CreatedAt,
+    val isDeleted: Int,
+    val memberId: Int,
+    val nickname: String,
+    val productId: Int,
+    val replyComment: String,
+    val replyId: Int,
+    val updatedAt: UpdatedAt,
+    val replyImg: String,
+
+    val productImage: String,
+    val productBrand: String,
+    val productName: String
+) {
+    data class CreatedAt(
+        val chronology: Chronology,
+        val dayOfMonth: Int,
+        val dayOfWeek: String,
+        val dayOfYear: Int,
+        val hour: Int,
+        val minute: Int,
+        val month: String,
+        val monthValue: Int,
+        val nano: Int,
+        val second: Int,
+        val year: Int
+    ) {
+        data class Chronology(
+            val calendarType: String,
+            val id: String
+        )
+    }
+
+    data class UpdatedAt(
+        val chronology: Chronology,
+        val dayOfMonth: Int,
+        val dayOfWeek: String,
+        val dayOfYear: Int,
+        val hour: Int,
+        val minute: Int,
+        val month: String,
+        val monthValue: Int,
+        val nano: Int,
+        val second: Int,
+        val year: Int
+    ) {
+        data class Chronology(
+            val calendarType: String,
+            val id: String
+        )
+    }
+}
